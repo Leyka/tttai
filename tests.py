1dec6eee363819e9417106c625a1d3499022c2fa
Skander <skander.kc@gmail.com>
2018-02-17 13:47:32 -0500

    Making tests work again

--- tests.py
+++ tests.py
@@ -8,34 +8,31 @@ class BoardTest(TestCase):
 
     def test_game_tie(self):
         board = self.board
-        player = board.current_player
         # Complete 8/9 squares
         for i in range(8):
-            board.update_square(i, player)
+            board.update_square(i)
         self.assertFalse(board.is_tied())
         # Complete the last square
-        board.update_square(8, player)
+        board.update_square(8)
         self.assertTrue(board.is_tied())
         self.assertTrue(board.game_over)
 
     def test_check_win(self):
         board = self.board
-        player = board.current_player
-        board.update_square(0, player)
-        board.update_square(1, player)
-        board.update_square(3, player)
+        board.update_square(0)
+        board.update_square(1)
+        board.update_square(3)
         # Should not win
-        self.assertFalse(board.check_win(player))
+        self.assertFalse(board.check_win())
         # Should win
-        board.update_square(2, player)
-        self.assertTrue(board.check_win(player))
+        board.update_square(2)
+        self.assertTrue(board.check_win())
         self.assertTrue(board.game_over)
-    """
+
     def test_next_player(self):
         board = self.board
         first_player = board.current_player
         second_player = CROSS if first_player == CIRCLE else CIRCLE
-        board.move(0)
+        board.play(0)
         # Check if player has changed
         self.assertEqual(board.current_player, second_player)
-    """
